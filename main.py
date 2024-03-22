20409af48f801446e7a68c826734c9aff7504423
agayushi1503 <148641522+agayushi1503@users.noreply.github.com>
2024-03-22 13:10:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,68 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import logging
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key'
-app.logger.setLevel(logging.INFO)
+app.secret_key = 'yoursecretkey'
+logging.basicConfig(level=logging.DEBUG)
 
-board = [['', '', ''], ['', '', ''], ['', '', '']]
+game_board = [['', '', ''], ['', '', ''], ['', '', '']]
+player1_turn = True
+winner = None
 
 @app.route('/')
 def index():
-    return render_template('index.html', board=board)
+    return render_template('index.html', game_board=game_board, player1_turn=player1_turn, winner=winner)
 
-@app.route('/process_move', methods=['POST'])
-def process_move():
-    row = int(request.form['row'])
-    col = int(request.form['col'])
-    player = request.form['player']
+@app.route('/make_move', methods=['POST'])
+def make_move():
+    x = int(request.form['x'])
+    y = int(request.form['y'])
 
-    if board[row][col] == '':
-        board[row][col] = player
-        winner = check_winner()
+    if not is_valid_move(x, y):
+        flash('Invalid move')
+        return redirect(url_for('index'))
 
-        if winner:
-            return render_template('index.html', board=board, winner=winner)
-        elif check_draw():
-            return render_template('index.html', board=board, draw=True)
+    make_move(x, y, player1_turn)
+    player1_turn = not player1_turn
+
+    winner = get_winner()
+    if winner:
+        flash(f'{winner} wins!')
+        return redirect(url_for('index'))
 
     return redirect(url_for('index'))
 
-def check_winner():
-    for row in board:
+@app.route('/reset')
+def reset():
+    global game_board, player1_turn, winner
+    game_board = [['', '', ''], ['', '', ''], ['', '', '']]
+    player1_turn = True
+    winner = None
+    return redirect(url_for('index'))
+
+def is_valid_move(x, y):
+    return game_board[x][y] == ''
+
+def make_move(x, y, player):
+    game_board[x][y] = player
+
+def get_winner():
+    for row in game_board:
         if row[0] == row[1] == row[2] and row[0] != '':
             return row[0]
 
     for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != '':
-            return board[0][col]
+        if game_board[0][col] == game_board[1][col] == game_board[2][col] and game_board[0][col] != '':
+            return game_board[0][col]
 
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != '':
-        return board[0][0]
+    if game_board[0][0] == game_board[1][1] == game_board[2][2] and game_board[0][0] != '':
+        return game_board[0][0]
 
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != '':
-        return board[0][2]
+    if game_board[0][2] == game_board[1][1] == game_board[2][0] and game_board[0][2] != '':
+        return game_board[0][2]
 
     return None
-
-def check_draw():
-    for row in board:
-        for cell in row:
-            if cell == '':
-                return False
-
-    return True
 
 if __name__ == '__main__':
     app.run(debug=True)
